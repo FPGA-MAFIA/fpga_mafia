fda2923f5b326f92776e03c1492aa9ab3b6e7c0a
amichaibd <amichaibd@gmail.com>
2023-01-16 22:13:03 +0200

    Enable the Cache word wr/rd hit in pipe

--- source/cache/cache_tq.sv
+++ source/cache/cache_tq.sv
@@ -19,32 +19,34 @@ module  cache_tq
     input   logic           clk,        
     input   logic           rst,        
     //core Interface
-    input   var t_req           core2cache_req,
+    input   var t_req       core2cache_req,
     output  logic           stall,
     output  t_rd_rsp        cache2core_rsp,
     //FM Inteface
-    input   var t_fm_rd_rsp     fm2cache_rd_rsp,
+    input   var t_fm_rd_rsp fm2cache_rd_rsp,
     //output 
     //Pipe Interface
     output  t_lu_req        pipe_lu_req_q1,
-    input   var t_lu_rsp        pipe_lu_rsp_q3
+    input   var t_lu_rsp    pipe_lu_rsp_q3
 );
 t_tq_state tq_state;
 t_tq_state next_tq_state;
-    assign stall            = '0;
-    //assign cache2core_rsp   = '0;
+assign stall            = '0;
 
 assign pipe_lu_req_q1.valid   = core2cache_req.valid;
 assign pipe_lu_req_q1.lu_op   = (core2cache_req.opcode == WR_OP) ? WR_LU :
                                 (core2cache_req.opcode == RD_OP) ? RD_LU :
                                                                    NO_LU ;
 assign pipe_lu_req_q1.address = core2cache_req.address;
-assign pipe_lu_req_q1.cl_data = core2cache_req.data;
+assign pipe_lu_req_q1.cl_data = '0;
+assign pipe_lu_req_q1.data    = core2cache_req.data;
 assign pipe_lu_req_q1.tq_id   = 5'd3;//FIXME
 
 
 
-assign cache2core_rsp.valid = pipe_lu_rsp_q3.valid;
+assign cache2core_rsp.valid =   pipe_lu_rsp_q3.valid                && 
+                                (pipe_lu_rsp_q3.lu_opcode == RD_LU) &&
+                                (pipe_lu_rsp_q3.lu_result == HIT);
 //take the relevant word from cacheline
 assign cache2core_rsp.data    = pipe_lu_rsp_q3.address[3:2] == 2'b00 ?  pipe_lu_rsp_q3.data[31:0]  : 
                                 pipe_lu_rsp_q3.address[3:2] == 2'b01 ?  pipe_lu_rsp_q3.data[63:32] : 
