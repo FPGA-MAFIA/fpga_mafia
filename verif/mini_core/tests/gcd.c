a96132c7c5d56239aad4d130b1dfbfe41d5db70a
amichai-bd <amichaibd@gmail.com>
2023-01-22 22:37:15 +0200

    small fix in mini_core tb

---
+++ verif/mini_core/tests/gcd.c
@@ -0,0 +1,14 @@
+#include "../../../app/defines/big_core_defines.h"
+
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+int main() {
+    int a = 48, b = 18;
+    int gcd_res = gcd(a, b);
+    MEM_SCRATCH[0]=gcd_res;
+    return 0;
+}
+
