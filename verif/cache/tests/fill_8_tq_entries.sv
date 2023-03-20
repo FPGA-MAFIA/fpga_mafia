dd3242fb84c842edc2e86c9bcafd500001773c6e
Noam <sabban.noam@gmail.com>
2023-03-20 11:45:06 +0200

    close #178; created python script that order ref_trk file by pairs of req/resp and integrated it to cache_pp

--- verif/cache/tests/fill_8_tq_entries.sv
+++ verif/cache/tests/fill_8_tq_entries.sv
@@ -14,12 +14,12 @@ delay(0); wr_req(20'h10_8_0, 32'h0000_8888, 5'd8); // write miss - send fill
 delay(10);
 //read the data back
 //FIXME - one issue #178 is resolved, change to delay(0) for all
-delay(2); rd_req(20'h10_1_0, 5'd9); // read hit
-delay(2); rd_req(20'h10_2_0, 5'd10); // read hit
-delay(2); rd_req(20'h10_3_0, 5'd11); // read hit
-delay(2); rd_req(20'h10_4_0, 5'd12); // read hit
-delay(2); rd_req(20'h10_5_0, 5'd13); // read hit
-delay(2); rd_req(20'h10_6_0, 5'd14); // read hit
-delay(2); rd_req(20'h10_7_0, 5'd15); // read hit
-delay(2); rd_req(20'h10_8_0, 5'd16); // read hit
+delay(0); rd_req(20'h10_1_0, 5'd9); // read hit
+delay(0); rd_req(20'h10_2_0, 5'd10); // read hit
+delay(0); rd_req(20'h10_3_0, 5'd11); // read hit
+delay(0); rd_req(20'h10_4_0, 5'd12); // read hit
+delay(0); rd_req(20'h10_5_0, 5'd13); // read hit
+delay(0); rd_req(20'h10_6_0, 5'd14); // read hit
+delay(0); rd_req(20'h10_7_0, 5'd15); // read hit
+delay(0); rd_req(20'h10_8_0, 5'd16); // read hit
 
