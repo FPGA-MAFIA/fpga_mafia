969dcf58fbeba59c85d6e2ebee0582e4b99a77d3
ShmuelSfez <shmuel.sfez@gmail.com>
2023-04-02 10:18:20 +0300

    small fixes

---
+++ verif/router/tests/simple.sv
@@ -0,0 +1,11 @@
+/*var t_tile_trans simple_trans [3:0];
+automatic randomize(seed = $random);
+for (int i = 0; i<4; i++)begin
+    randomize(simple_trans[i]);
+    push_fifo(i);
+    delay(3);
+end*/
+for (int i = 0; i<4; i++)begin
+    push_fifo(i);
+    delay(3);
+end
---
+++ verif/router/tests/single_fifo_full_BW.sv
@@ -0,0 +1,1 @@
+parameter REQUESTS = 10;
