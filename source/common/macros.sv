04426c601da0d8f64d2299bd0876246617b56f48
ShmuelSfez <shmuel.sfez@gmail.com>
2023-05-03 22:38:35 +0300

    fixed macro.

--- source/common/macros.sv
+++ source/common/macros.sv
@@ -51,10 +51,10 @@
     end                                        
 
 
-`define  ENCODER(encoded , decoded )             \
+`define  ENCODER(encoded ,valid, decoded )             \
 	always_comb begin                                   \
         encoded = '0 ;                                 	\
-        //valid   = |decoded;                             \
+        valid   = |decoded;                             \
         for (int i = 0 ; i <$bits(decoded) ;i++) begin  \
 	        if (decoded[i])                             \
     	        encoded = i ;                           \
