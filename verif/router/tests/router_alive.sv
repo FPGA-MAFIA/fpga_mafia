c117bd591afb8fc964e9589d7d8fb038da6930fb
Yeonatan Perelman <yp100002@gmail.com>
2023-05-11 21:36:13 +0300

    randomize_task router

--- verif/router/tests/router_alive.sv
+++ verif/router/tests/router_alive.sv
@@ -1,13 +1,8 @@
 //First request
 delay(10);
-input_gen_valid[NORTH] = 1'b1;
-random_gen_req(.card(NORTH), .trans(input_gen[NORTH]));
-delay(1);
-input_gen_valid[NORTH] = 1'b0;
-
+random_gen_req(.input_card(WEST));
 //Second request
 delay(10);
-input_gen_valid[NORTH] = 1'b1;
-random_gen_req(.card(NORTH), .trans(input_gen[NORTH]));
-delay(1);
-input_gen_valid[NORTH] = 1'b0;
+random_gen_req(.input_card(SOUTH)); 
+delay(10);
+random_gen_req(.input_card(SOUTH)); 
