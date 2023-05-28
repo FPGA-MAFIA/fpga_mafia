89c0f762b093be30ce2dd2a78a4272db9efd668f
amichaibd <amichaibd@gmail.com>
2023-05-28 22:38:48 +0300

    fds

--- verif/fabric/tests/fabric_alive.sv
+++ verif/fabric/tests/fabric_alive.sv
@@ -1,18 +1,4 @@
 $display("Finished elaborating the design...");
-
-//delay(10);
-//send_rand_req(.source_id(8'h1_1), .target_id(8'h3_3));
-//delay(10);
-//send_rand_req(.source_id(8'h1_2), .target_id(8'h3_3));
-//delay(10);
-//send_rand_req(.source_id(8'h2_1), .target_id(8'h3_3));
-//delay(10);
-//send_rand_req(.source_id(8'h3_3), .target_id(8'h1_1));
-//delay(10);
-//send_rand_req(.source_id(8'h1_3), .target_id(8'h3_1));
-//delay(10);
-
-
 for(int i=0; i< 10; i++) begin
     do begin 
         `RAND_EP(rand_source)
