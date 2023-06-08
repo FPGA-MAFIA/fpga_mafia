f8fb5418c5e6efdc74d0ca3bbbed159e5f2c513b
amichai-bd <amichaibd@gmail.com>
2023-06-08 09:23:20 +0300

    move the source/router dir into the source/fabric fix the .f acordengly

--- source/fabric/router/router_rtl_list.f
+++ source/fabric/router/router_rtl_list.f
@@ -13,8 +13,8 @@
 +incdir+../../../source/common/
 //RTL FIles
 ../../../source/common/common_pkg.sv
-../../../source/router/router.sv
-../../../source/router/fifo_arb.sv
 ../../../source/common/fifo.sv
 ../../../source/common/arbiter.sv
-../../../source/router/next_tile_fifo_arb.sv
+../../../source/fabric/router/router.sv
+../../../source/fabric/router/fifo_arb.sv
+../../../source/fabric/router/next_tile_fifo_arb.sv
