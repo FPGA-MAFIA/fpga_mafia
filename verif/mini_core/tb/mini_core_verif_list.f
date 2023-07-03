457602348edcd0eae7c7a1c61f37468065c5f1d9
amichai-bd <amichaibd@gmail.com>
2023-07-03 23:23:30 +0300

    fix mini core f file for ref model

--- verif/mini_core/tb/mini_core_verif_list.f
+++ verif/mini_core/tb/mini_core_verif_list.f
@@ -1,3 +1,4 @@
 +incdir+../../../source/common/
 ../../../verif/mini_core/tb/mini_core_tb.sv
-../../../verif/rv32i_ref/rv32i_ref.sv
+../../../verif/rv32i_ref/tb/rv32i_ref_pkg.sv
+../../../verif/rv32i_ref/tb/rv32i_ref.sv
