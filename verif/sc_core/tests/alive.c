69a54f847a447b5f7e942b580547f403de2978c5
roman022285 <roman329@gmail.com>
2024-01-14 20:49:52 +0200

    add tests to sc_core

--- verif/sc_core/tests/alive.c
+++ verif/sc_core/tests/alive.c
@@ -1,17 +1,11 @@
-#define VGA_MEM_BASE 0x00FF0000
-#include "fabric_defines.h"
-#include "big_core_defines.h"
-#include "graphic_vga.h"
+int sum(int x, int y){
+    return x + y;
+}
 
-int main()  {  
-    int x,y,z;  
-    x = 2;  
-    y = 3;  
-    z = x+y;  
-
-rvc_printf("HI\n");
-//rvc_printf("THIS IS POC FOR DANIEL\n");
-//rvc_printf("NEXT LINE AGAIN\n");
-//rvc_printf("A REALLY LONG LINE - I WANT TO SEE IF THE PRINT KNOWS HOW TO MOVE TO THE NEXT LINE ALL BY IT SELF \n");
-
-}  // main()
+int main(){
+    int x = 1;
+    int y = 2;
+  
+    int z = sum(x, y); 
+    return 0;
+}
