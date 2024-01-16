370428bad12b0462d6eb45b029f49381722a5704
roman022285 <roman329@gmail.com>
2024-01-16 22:35:08 +0200

    update mtval in rv32i_ref model

--- verif/core_rrv/tests/alive_illegal.c
+++ verif/core_rrv/tests/alive_illegal.c
@@ -3,9 +3,15 @@ int main() {
 
     int x = 1;
     int y = 2;
-    //This instruction is trying to shift to an illegal amount of bits.
+    
+    // This instruction is trying to shift to an illegal amount of bits.
     asm(".word 0xfff79793" : /* outputs / : / inputs / : / clobbers */);
     int z = x + y;
+
+    // Reading mepc and mtval 
+    int csr_mepc, csr_mtval;
+    asm volatile ("csrr %0, 0x341" : "=r" (csr_mepc));
+    asm volatile ("csrr %0, 0x343" : "=r" (csr_mtval)); 
 
     return 0;
 }
