9e79d06bfdbb867d879a1dfa6db4d8e2498ee03e
roman022285 <roman329@gmail.com>
2024-01-26 17:52:31 +0200

    refactor TimerInterruptQ101 in ctrl

--- source/core_rrv/core_rrv_ctrl.sv
+++ source/core_rrv/core_rrv_ctrl.sv
@@ -91,9 +91,19 @@ assign PreRegSrc2Q101H           = PreInstructionQ101H[24:20];
 logic IllegalInstructionQ101H;
 assign IllegalInstructionQ101H = (PreIllegalInstructionQ101H) && ! (flushQ102H || flushQ103H);
 
+logic PreTimerInterruptQ101H;
+assign PreTimerInterruptQ101H = (CsrTimerInterruptQ102H.csr_custom_mtime == CsrTimerInterruptQ102H.csr_custom_mtimecmp);
+
+logic CsrMstatusMieBit, CsrMieMieBit, TimerInterruptEnable;
+assign CsrMstatusMieBit = CsrTimerInterruptQ102H.csr_mstatus[3];
+assign CsrMieMieBit     = CsrTimerInterruptQ102H.csr_mie[7];
+assign TimerInterruptEnable = CsrMstatusMieBit & CsrMieMieBit;
+
 logic TimerInterruptQ101H;
-assign TimerInterruptQ101H = (CsrTimerInterruptQ102H.csr_mstatus[3] && CsrTimerInterruptQ102H.csr_mie[7]) &&
-                              (CsrTimerInterruptQ102H.csr_custom_mtime == CsrTimerInterruptQ102H.csr_custom_mtimecmp);
+//assign TimerInterruptQ101H = (CsrTimerInterruptQ102H.csr_mstatus[3] && CsrTimerInterruptQ102H.csr_mie[7]) &&
+//                              (CsrTimerInterruptQ102H.csr_custom_mtime == CsrTimerInterruptQ102H.csr_custom_mtimecmp);
+
+assign TimerInterruptQ101H = PreTimerInterruptQ101H & TimerInterruptEnable;
 
 logic LoadHazardValidRegSrc2Q101H;
 logic RegDstQ102MatchRegSrc1Q101H;
