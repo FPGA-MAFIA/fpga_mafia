7f6a48cbb03213ae0a3ef1de8cac99abc9dafbeb
roman022285 <roman329@gmail.com>
2024-02-12 22:26:39 +0200

    fix the type of illegal instruction explanation

--- verif/core_rrv/tests/alive_illegal.c
+++ verif/core_rrv/tests/alive_illegal.c
@@ -4,7 +4,7 @@ int main() {
     int x = 1;
     int y = 2;
     
-    // This instruction is trying to shift to an illegal amount of bits.
+    // This instruction is trying to generate slli instruction with illegal FUNCT7.
     asm(".word 0xfff79793" : /* outputs / : / inputs / : / clobbers */);
     int z = x + y;
 
