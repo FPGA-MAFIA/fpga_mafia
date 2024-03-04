4af4d1124f333d552f2cd727b971398d280040e5
roman022285 <roman329@gmail.com>
2024-03-04 15:30:22 +0200

    refactor ps2_grey_kbd_fifo_tb

--- verif/ps2_kbd/tb/ps2_kbd_grey_fifo_tb.sv
+++ verif/ps2_kbd/tb/ps2_kbd_grey_fifo_tb.sv
@@ -23,14 +23,25 @@ logic [7:0] data_in, data_out;
 logic write_en, read_en;
 logic empty, full;
 
+logic [7:0] fifo_data[0:7];
+initial begin : data_init
+    fifo_data[0] = 8'h00;
+    fifo_data[1] = 8'h01;
+    fifo_data[2] = 8'h02;
+    fifo_data[3] = 8'h03;
+    fifo_data[4] = 8'h04;
+    fifo_data[5] = 8'h05;
+    fifo_data[6] = 8'h06;
+    fifo_data[7] = 8'h07;
+end : data_init
 
 //=======================================
 // keyboard clock generator (slow domain)
 //======================================
 initial begin :kb2_clk_generator
     forever begin
-        #5 kbd_clk = 1'b0;
-        #5 kbd_clk = 1'b1;
+        #10 kbd_clk = 1'b0;
+        #10 kbd_clk = 1'b1;
     end
 end
 
@@ -53,7 +64,7 @@ initial begin : rst_generator
 end
 
 
-ps2_kbd_grey_fifo #(.DEPTH(16), .WIDTH()) ps2_kbd_grey_fifo(
+ps2_kbd_grey_fifo #(.DEPTH(8), .WIDTH(8)) ps2_kbd_grey_fifo(
     .kbd_clk(kbd_clk),
     .cc_clk (cc_clk), 
     .data_in(data_in),
@@ -69,22 +80,23 @@ logic [3:0] data;
 
 initial begin :main
     #100;
-    for(data = 0; data <=4'hf; data++) begin : write_to_fifo
-       write_en = 1'b1;
-       @(posedge kbd_clk); 
-       data_in = data;
-       $display("empty: %1h, full = %1h", empty, full);
-       if(full)
-        break; 
-    end
+    push_data(fifo_data[0]);
+    push_data(fifo_data[1]);
+    push_data(fifo_data[2]);
+    push_data(fifo_data[3]);
+    push_data(fifo_data[4]);
+    push_data(fifo_data[5]);
+    push_data(fifo_data[6]);
+    push_data(fifo_data[7]);
 
-    while(!empty) begin: read_from_fifo
-      read_en = 1'b1;
-      write_en = 1'b0;
-      @(posedge cc_clk);
-      $display("data_out: %1h", data_out);   
-      #10;  
-    end
+    pop_data();
+    pop_data();
+    pop_data();
+    pop_data();
+    pop_data();
+    pop_data();
+    pop_data();
+    pop_data();
     
     $finish;
 
@@ -96,4 +108,18 @@ initial begin : time_out
     $display("finished with timeout");
     $finish;
 end
+
+task push_data(input logic[7:0] data);
+    write_en = 1'b1;
+    @(posedge kbd_clk);
+    data_in = data;
+    #20; 
+endtask
+
+task pop_data();
+      read_en = 1'b1;
+      @(posedge cc_clk);
+      #10;  
+endtask
+
 endmodule
