1bf7b98f4c5e96379823ee6b6ce65e0d2368ef16
amichai-bd <amichaibd@gmail.com>
2024-03-06 01:24:41 +0200

    small fix

--- verif/core_rrv/tests/PS2_alive.c
+++ verif/core_rrv/tests/PS2_alive.c
@@ -3,30 +3,13 @@
 #include "graphic_vga.h"
 int main(){
 
-char str[12];
+char str[30];
 
 rvc_printf("ENTER A STRING:\n> ");
-rvc_scanf(str, 12);
-rvc_printf("\n YOU ENTERED:  ");
+rvc_scanf(str, 30);
+rvc_printf("\nYOU ENTERED: ");
 rvc_printf(str);
 
-//rvc_printf("\n");
-
-// manually cast and zero extend the  char to int
-//int a_char = (int)str[0];
-//int b_char = (int)str[1];
-//int c_char = (int)str[2];
-//int d_char = (int)str[3];
-//
-//
-//rvc_print_unsigned_int_hex(a_char);
-//rvc_printf("\n");
-//rvc_print_unsigned_int_hex(b_char);
-//rvc_printf("\n");
-//rvc_print_unsigned_int_hex(c_char);
-//rvc_printf("\n");
-//rvc_print_unsigned_int_hex(d_char);
-//rvc_printf("\n");
 
 return 0;
 }
