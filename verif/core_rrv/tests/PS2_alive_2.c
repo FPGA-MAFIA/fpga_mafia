1b67771f74b2bd0191e05d67105cc2b1edee5e81
roman022285 <roman329@gmail.com>
2024-03-09 17:18:23 +0200

    add tasks and test allowing KBD to recieve ascii insted of keycode

---
+++ verif/core_rrv/tests/PS2_alive_2.c
@@ -0,0 +1,22 @@
+
+#include "big_core_defines.h"
+#include "graphic_vga.h"
+#include "interrupt_handler.h"
+int main(){
+
+char str1[30];
+char str2[30];
+
+rvc_printf("ENTER A STRING1:\n> ");
+rvc_scanf(str1, 30);
+rvc_printf("\nYOU ENTERED: ");
+rvc_printf(str1);
+
+rvc_printf("\nENTER A STRING2:\n> ");
+rvc_scanf(str2, 30);
+rvc_printf("\nYOU ENTERED: ");
+rvc_printf(str2);
+
+
+return 0;
+}
