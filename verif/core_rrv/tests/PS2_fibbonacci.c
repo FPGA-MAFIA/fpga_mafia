18446a3069e937c1de3b6aad7a00a3cf9abecdb8
roman022285 <roman329@gmail.com>
2024-03-13 09:10:17 +0200

    add string library

--- verif/core_rrv/tests/PS2_fibbonacci.c
+++ verif/core_rrv/tests/PS2_fibbonacci.c
@@ -1,8 +1,7 @@
 #include "big_core_defines.h"
 #include "graphic_vga.h"
+#include "string.h"
 
-int atoi(const char *str);
-int strlen(const char *str);
 int fibbonacci(int n);
 
 int main() {
@@ -20,39 +19,6 @@ int main() {
     rvc_printf("\n");
    
     return 0;
-}
-
-int atoi(const char *str) {
-    int result = 0;
-    int sign = 1;
-    int i = 0;
-    
-    // Skip leading whitespaces
-    while (str[i] == ' ') {
-        i++;
-    }
-    
-    // Check for sign
-    if (str[i] == '+' || str[i] == '-') {
-        sign = (str[i++] == '-') ? -1 : 1;
-    }
-    
-    // Convert digits to integer
-    while (str[i] >= '0' && str[i] <= '9') {
-        result = result * 10 + (str[i++] - '0');
-    }
-    
-    return sign * result;
-}
-
-int strlen(const char *str) {
-    int len = 0;
-    
-    while (str[len] != '\0') {
-        len++;
-    }
-    
-    return len;
 }
 
 int fibbonacci(int n) {
