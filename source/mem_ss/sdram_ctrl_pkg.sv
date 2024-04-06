6c12e760af85b9dd405583dba0cdfaf1f1856b7b
roman022285 <roman329@gmail.com>
2024-04-06 18:38:45 +0300

    sdram state machine with initialization states

--- source/mem_ss/sdram_ctrl_pkg.sv
+++ source/mem_ss/sdram_ctrl_pkg.sv
@@ -3,17 +3,22 @@ package sdram_ctrl_pkg;
 // The following parameters are for de10-lite 32MBx16 SDRAM
 // Grade speed -5 with 200MHz clock. 
 
-// The parameters were taken from the spec on Pg.19-20 
-parameter  tRCDCounter = 3;  // Activate command to READ/WRITE. 3 clock cycles (15ns) 
-parameter  tCACCounter = 3;  // CAS latency counter. Read latency. 3 clock cycles (15ns)
-parameter  tRASCounter = 7;  // Activation to Precharge. 7 clock cycles (35ns)
-parameter  tRPCounter  = 3;  // Precharge to Activation. 3 clock cycles (15ns)
-parameter  tRCCounter  = 10; // Refresh to refresh. 10 clock cycles (50ns)
-parameter  RefCounter  = 1560; // Refresh time. tref = 64ms -> 64ms/8192 rows    
+// Timing parameters
+parameter  tRCDMax     = 3;     // Activate command to READ/WRITE. 3 clock cycles (15ns) 
+parameter  tCACMax     = 3;     // CAS latency counter. Read latency. 3 clock cycles (15ns)
+parameter  tRASMax     = 7;     // Activation to Precharge. 7 clock cycles (35ns)
+parameter  tRPMax      = 3;     // Precharge to Activation(cycles to perform precharge). 3 clock cycles (15ns)
+parameter  tRCMax      = 10;    // Refresh to refresh(cycles to perform refresh). 10 clock cycles (50ns)
+parameter  tMRDMax     = 10;    // Mode register program time.
+parameter  RefreshMax  = 1560;  // Refresh time. tref = 64ms -> 64ms/8192 rows 
 
-parameter ModeRegistrSet = 15'b00000_0_00_011_0_011; // set sequential burst of lenght 8. set CAS latency to 3 cycles(for 200Mhz op). Set bit 9 to 1 for single Location access
 
+parameter InhibitMax               = 20000;                    // when power up no operation should be taken during that period (100us/5ns)
+parameter InitRefreshMax           = 80;                       // 8 refresh cycles are needed at power up. Each one is tRCMax   
+parameter ModeRegAddrBitsSetBurst  = 13'b000_0_00_011_0_011; // set sequential burst of lenght 8. set CAS latency to 3 cycles(for 200Mhz op). 
+parameter ModeRegAddrBitsSetSingle = 13'b000_1_00_011_0_000;
 
+/*
 typedef struct packed {
     logic   [12:0]  DRAM_ADDR;  
 	logic	[1:0]	DRAM_BA;
@@ -27,17 +32,21 @@ typedef struct packed {
 	logic		    RAM_UDQM;
     logic           DRAM_WE_N;
 } sdram_ctrl;
+*/
 
 typedef enum {
-   INIT,        // Initialize state after power up
-   IDLE,        // Waiting for commands
-   MRS,         // Mode register set. 
-   ACTIVE,      // Row is activated 
-   READ,        // Performing read operation
-   WRITE,       // Performing write operation
-   PRECHARGE,   // Precharging 
-   REFRESH,     // Perform auti-refresh cycle
-   WAIT_REFRESH // Waiting to complete current command befire starting refresh   
+   // for INIT states details please refer to the documenttion Pages.22-23 
+   INIT_NOP,       // Initialize state after power up
+   INIT_PALL,      // precharge all banks at init stage
+   INIT_REFRESH,   // performe refresh at init state
+   /////////////////////////////////////////////////////////////////////
+   IDLE,           // Waiting for commands
+   MRS,            // Mode register set. 
+   ACTIVE,         // Row is activated 
+   READ,           // Performing read operation
+   WRITE,          // Performing write operation
+   PRECHARGE,      // Precharging 
+   REFRESH        // Perform auti-refresh cycle
 } sdram_states;
 
 endpackage
