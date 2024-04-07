8c965a8726e3fe4650f3d937163d4d94876d0a7b
roman022285 <roman329@gmail.com>
2024-04-07 15:31:50 +0300

    refactor the state machine

--- source/mem_ss/sdram_ctrl_pkg.sv
+++ source/mem_ss/sdram_ctrl_pkg.sv
@@ -18,35 +18,17 @@ parameter InitRefreshMax           = 80;                       // 8 refresh cycl
 parameter ModeRegAddrBitsSetBurst  = 13'b000_0_00_011_0_011; // set sequential burst of lenght 8. set CAS latency to 3 cycles(for 200Mhz op). 
 parameter ModeRegAddrBitsSetSingle = 13'b000_1_00_011_0_000;
 
-/*
-typedef struct packed {
-    logic   [12:0]  DRAM_ADDR;  
-	logic	[1:0]	DRAM_BA;
-    logic           DRAM_CAS_N;
-    logic           DRAM_CKE;
-    logic           DRAM_CLK;
-	logic     		DRAM_CS_N;
-	logic  [15:0]	DRAM_DQ;
-	logic		    DRAM_LDQM;
-    logic	        DRAM_RAS_N;
-	logic		    RAM_UDQM;
-    logic           DRAM_WE_N;
-} sdram_ctrl;
-*/
-
 typedef enum {
-   // for INIT states details please refer to the documenttion Pages.22-23 
-   INIT_NOP,       // Initialize state after power up
-   INIT_PALL,      // precharge all banks at init stage
-   INIT_REFRESH,   // performe refresh at init state
-   /////////////////////////////////////////////////////////////////////
+   INIT            // Initial state
    IDLE,           // Waiting for commands
    MRS,            // Mode register set. 
-   ACTIVE,         // Row is activated 
+   ACTIVATE,       // Row is activated 
    READ,           // Performing read operation
    WRITE,          // Performing write operation
    PRECHARGE,      // Precharging 
-   REFRESH        // Perform auti-refresh cycle
+   REFRESH,        // Perform auti-refresh cycle
+   NOP,            // NOP              
+   PRECHARGE_ALL            // Precharge all
 } sdram_states;
 
 endpackage
