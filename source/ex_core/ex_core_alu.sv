f9382af58cd5f13fabf027840d320b709326495a
Noam Rahat <noamrht@gmail.com>
2024-04-28 17:26:53 +0300

    Complete ALU sum-module #553 suports: ADD, SUB, AND, OR, XOR, SLL: - Shift Left Logical, SRL: - Shift Right Logical, SRA: - Shift Right Arithmetic, SLT: - Set Less Than, SLTU: - Set Less Than Unsigned
    Signed-off-by: Noam Rahat <noamrht@gmail.com>

--- source/ex_core/ex_core_alu.sv
+++ source/ex_core/ex_core_alu.sv
@@ -1,14 +1,22 @@
+// ./build.py -sim -dut ex_core -top ex_core_alu_tb -hw -clean
 module ex_core_alu(input logic [31:0] operand1,
            input logic [31:0] operand2,
-           input logic [2:0] op,
+           input logic [3:0] op,
            output logic [31:0] result,
            output logic zero);
 
    always_comb begin
       case(op)
-         3'b000: result = operand1 + operand2; // ADD
-         3'b001: result = operand1 - operand2; // SUB
-         // Other operations...
+         4'b0000: result = operand1 + operand2; // ADD
+         4'b0001: result = operand1 - operand2; // SUB
+         4'b0010: result = operand1 & operand2; // AND
+         4'b0011: result = operand1 | operand2; // OR
+         4'b0100: result = operand1 ^ operand2; // XOR
+         4'b0101: result = operand1 << operand2; // SLL
+         4'b0110: result = operand1 >> operand2; // SRL
+         4'b0111: result = operand1 >>> operand2; // SRA
+         4'b1000: result = (operand1 < operand2) ? 1 : 0; // SLT
+         4'b1001: result = (operand1 < operand2) ? 1 : 0; // SLTU
          default: result = 0; // Default case
       endcase
       zero = (result == 0);
--- verif/ex_core/tb/ex_core_alu_tb.sv
+++ verif/ex_core/tb/ex_core_alu_tb.sv
@@ -1,49 +1,75 @@
+// ./build.py -sim -dut ex_core -top ex_core_alu_tb -hw -clean
 module ex_core_alu_tb();
 
-   // Define signals
-   logic [31:0] operand1, operand2;
-   logic [2:0] op;
-   logic [31:0] result;
-   logic zero;
-   
-   // Instantiate ALU
-   ex_core_alu dut(.operand1(operand1), .operand2(operand2),
-           .op(op), .result(result), .zero(zero));
-   
-   // Apply stimulus
-   initial begin
-      // Test case 1: ADD operation
-      operand1 = 10;
-      operand2 = 5;
-      op = 3'b000; // ADD
-      #10; // Delay for combinational logic to settle
-      $display("The result of %1h ADD %1h is %1h", operand1, operand2, result);
-      // Verify result
-      //assert (result === 15) else $error("ADD test failed!");
+    // Define signals
+    logic [31:0] operand1, operand2;
+    logic [3:0] op;
+    logic [31:0] result;
+    logic zero;
 
-      // Other test cases...
-   end
-   
-   // Random stimulus
-   initial begin
-      #10
-      repeat (10) begin
-         operand1 = $urandom_range(0, 10);
-         operand2 = $urandom_range(0, 10);
-         op = $urandom_range(0, 7); // Generate a random operation code
-         #10;
-         $display("The result of %1h and %1h with op %1h is %1h", operand1, operand2, op, result);
-         // Check result (this will depend on the operation)
-      end
-      $finish;
-   end
+    // Define opcode as a word
+    typedef enum logic [3:0] {
+        ADD  = 4'b0000,
+        SUB  = 4'b0001,
+        AND  = 4'b0010,
+        OR   = 4'b0011,
+        XOR  = 4'b0100,
+        SLL  = 4'b0101,
+        SRL  = 4'b0110,
+        SRA  = 4'b0111,
+        SLT  = 4'b1000,
+        SLTU = 4'b1001
+    } opcode_t;
 
-   parameter V_TIMEOUT = 10000;
+    opcode_t opcode;
 
-   initial begin: detec_timeout
-      #V_TIMEOUT
-      $error("time out reached");
-      $finish;
-   end
+    // Define opcode_name function
+    function string opcode_name(opcode_t opcode);
+        case (opcode)
+            ADD:  return "ADD";
+            SUB:  return "SUB";
+            AND:  return "AND";
+            OR:   return "OR";
+            XOR:  return "XOR";
+            SLL:  return "SLL";
+            SRL:  return "SRL";
+            SRA:  return "SRA";
+            SLT:  return "SLT";
+            SLTU: return "SLTU";
+            default: return "UNKNOWN";
+        endcase
+    endfunction
+
+    // Instantiate ALU
+    ex_core_alu dut(
+    .operand1   (operand1),
+    .operand2   (operand2),
+    .op         (op),
+    .result     (result),
+    .zero       (zero)
+    );
+
+    // Aplly Random stimulus
+    initial begin
+        #10
+        repeat (15) begin
+            operand1 = $urandom_range(0, 10);
+            operand2 = $urandom_range(0, 10);
+            opcode = opcode_t'($urandom_range(0, 9)); // Generate a random operation code
+            op = opcode;
+            #10;
+            $display("The result of %4b and %4b with operation %s is %4b", operand1, operand2, opcode_name(opcode), result);
+            // Check result (this will depend on the operation)
+        end
+        $finish;
+    end
+
+    parameter V_TIMEOUT = 10000;
+
+    initial begin: detec_timeout
+        #V_TIMEOUT
+        $error("time out reached");
+        $finish;
+    end
 
 endmodule
