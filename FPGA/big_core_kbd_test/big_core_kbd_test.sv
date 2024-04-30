98758a36d5e09b8f320e0b7611e216b156b06b88
roman022285 <roman329@gmail.com>
2024-04-30 21:03:34 +0300

    keyboard basic test design

---
+++ FPGA/big_core_kbd_test/big_core_kbd_test.sv
@@ -0,0 +1,31 @@
+module big_core_kbd_test(
+    input  logic MAX10_CLK1_50,
+    input  logic Rst,         // not neccessary at all
+    input  logic KbdClockIn,  // recieved clock from keyboard
+    input  logic KbdDataIn,   // recieved data from keyboard
+    
+    /*
+    when inputs are not connected the synthesizer ignores them
+    this is the reason why we make a naive assign just to capture
+    them in signal tapping
+    */
+    output logic Clock25Khz,
+    output logic KbdClockOut,
+    output logic KbdDataOut
+);
+
+    assign KbdClockOut = (Rst) ? KbdClockIn : 1'b0;
+    assign KbdDataOut  = (Rst) ? KbdDataIn  : 1'b0;
+
+// we use pll to create slower clock to capture keyboard signals in signal tapping
+ pll	pll_inst (
+	.areset (),
+	.inclk0 (MAX10_CLK1_50),
+	.c0 (Clock25Khz),
+	.locked ()
+	);
+
+
+
+endmodule
+
