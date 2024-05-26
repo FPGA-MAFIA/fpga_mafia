4e7b293e378c22700ca8237da9fda5d982d03b4f
Noam Rahat <noamrht@gmail.com>
2024-05-26 21:34:28 +0300

    [#553]: Pack and Move ALU opcode to ex_core_pkg.sv
    ALU opcode packed in t_alu_op enum.
    
    Signed-off-by: Noam Rahat <noamrht@gmail.com>

--- source/ex_core/ex_core_pkg.sv
+++ source/ex_core/ex_core_pkg.sv
@@ -1,5 +1,26 @@
 package ex_core_pkg;
 
+typedef enum logic [3:0] {
+    ADD  = 4'b0000,
+    SUB  = 4'b0001,
+    AND  = 4'b0010,
+    OR   = 4'b0011,
+    XOR  = 4'b0100,
+    SLL  = 4'b0101,
+    SRL  = 4'b0110,
+    SRA  = 4'b0111,
+    SLT  = 4'b1000,
+    SLTU = 4'b1001
+} t_alu_op;
+
+typedef struct packed {
+    t_alu_op AluOp;
+    logic [4:0] RegSrc1;
+    logic [4:0] RegSrc2;
+    logic [4:0] RegDst;
+    logic       RegWrEn;
+} t_ctrl_alu;
+
 typedef struct packed {
     logic [4:0] RegSrc1;
     logic [4:0] RegSrc2;
--- verif/ex_core/tb/ex_core_alu_tb.sv
+++ verif/ex_core/tb/ex_core_alu_tb.sv
@@ -1,50 +1,20 @@
 // ./build.py -sim -dut ex_core -top ex_core_alu_tb -hw -clean
+`include "ex_core_pkg.sv"
+
 module ex_core_alu_tb();
+import ex_core_pkg::*;
 
     // Define signals
     logic [31:0] operand1, operand2;
-    logic [3:0] op;
+    var t_ctrl_alu Ctrl;
     logic [31:0] result;
     logic zero;
-
-    // Define opcode as a word
-    typedef enum logic [3:0] {
-        ADD  = 4'b0000,
-        SUB  = 4'b0001,
-        AND  = 4'b0010,
-        OR   = 4'b0011,
-        XOR  = 4'b0100,
-        SLL  = 4'b0101,
-        SRL  = 4'b0110,
-        SRA  = 4'b0111,
-        SLT  = 4'b1000,
-        SLTU = 4'b1001
-    } opcode_t;
-
-    opcode_t opcode;
-
-    // Define opcode_name function
-    function string opcode_name(opcode_t opcode);
-        case (opcode)
-            ADD:  return "ADD";
-            SUB:  return "SUB";
-            AND:  return "AND";
-            OR:   return "OR";
-            XOR:  return "XOR";
-            SLL:  return "SLL";
-            SRL:  return "SRL";
-            SRA:  return "SRA";
-            SLT:  return "SLT";
-            SLTU: return "SLTU";
-            default: return "UNKNOWN";
-        endcase
-    endfunction
 
     // Instantiate ALU
     ex_core_alu dut(
     .operand1   (operand1),
     .operand2   (operand2),
-    .op         (op),
+    .Ctrl         (Ctrl),
     .result     (result),
     .zero       (zero)
     );
@@ -55,10 +25,9 @@ module ex_core_alu_tb();
         repeat (15) begin
             operand1 = $urandom_range(0, 10);
             operand2 = $urandom_range(0, 10);
-            opcode = opcode_t'($urandom_range(0, 9)); // Generate a random operation code
-            op = opcode;
+            Ctrl.AluOp = t_alu_op'($urandom_range(0, 9)); // Generate a random operation code
             #10;
-            $display("The result of %4b and %4b with operation %s is %4b", operand1, operand2, opcode_name(opcode), result);
+            $display("The result of %4b and %4b with operation %s is %4b", operand1, operand2, Ctrl.AluOp, result);
             // Check result (this will depend on the operation)
         end
         $finish;
