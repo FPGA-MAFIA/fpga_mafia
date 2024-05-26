674d8a39d0de5fe82c27c4355f75e050e2de8dc8
roman022285 <roman329@gmail.com>
2024-05-26 16:09:20 +0300

    add lb and lh test

--- verif/mem_ss/tests/write_read_partial_words.sv
+++ verif/mem_ss/tests/write_read_partial_words.sv
@@ -2,9 +2,9 @@
 delay(0);   wr_req_sb(20'hE8_01_0, 32'hDEAD_BEAF , 5'b0);
 delay(0);   wr_req_sh(20'hE9_01_0, 32'hFAFA_FAFA , 5'b1);
 delay(0);   rd_req_lbu(20'hE8_01_0, 5'd1); //FIXME support in HW signed and unsigned
-delay(0);   rd_req_lbu(20'hE8_01_0, 5'd1);
+delay(0);   rd_req_lb(20'hE8_01_0, 5'd1);
 delay(0);   rd_req_lhu(20'hE9_01_0, 5'd2); //FIXME support in HW signed and unsigned
-delay(0);   rd_req_lhu(20'hE9_01_0, 5'd2);
+delay(0);   rd_req_lh(20'hE9_01_0, 5'd2);
 
 // Same CL (same TAG 'E8', SAME SET '5'), different words (1,2,3):
 delay(0);   wr_req(20'hE8_05_4, 32'h4444_4444 , 5'b0);
