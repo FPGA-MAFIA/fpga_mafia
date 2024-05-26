40a5548b4f5ec2d196b6f0a915c4d9095c745a4f
Noam Rahat <noamrht@gmail.com>
2024-05-26 21:34:28 +0300

    [#553]: Pack and Move ALU opcode to ex_core_pkg.sv
    ALU opcode packed in t_alu_op enum.
    
    Signed-off-by: Noam Rahat <noamrht@gmail.com>

--- source/ex_core/ex_core_alu.sv
+++ source/ex_core/ex_core_alu.sv
@@ -1,22 +1,27 @@
 // ./build.py -sim -dut ex_core -top ex_core_alu_tb -hw -clean
-module ex_core_alu(input logic [31:0] operand1,
-           input logic [31:0] operand2,
-           input logic [3:0] op,
-           output logic [31:0] result,
-           output logic zero);
+`include "ex_core_pkg.sv"
+
+module ex_core_alu
+import ex_core_pkg::*;
+(
+   input logic [31:0] operand1,
+   input logic [31:0] operand2,
+   input logic [3:0] op,
+   output logic [31:0] result,
+   output logic zero);
 
    always_comb begin
       case(op)
-         4'b0000: result = operand1 + operand2; // ADD
-         4'b0001: result = operand1 - operand2; // SUB
-         4'b0010: result = operand1 & operand2; // AND
-         4'b0011: result = operand1 | operand2; // OR
-         4'b0100: result = operand1 ^ operand2; // XOR
-         4'b0101: result = operand1 << operand2; // SLL
-         4'b0110: result = operand1 >> operand2; // SRL
-         4'b0111: result = operand1 >>> operand2; // SRA
-         4'b1000: result = ($signed(operand1) < $signed(operand2)) ? 1 : 0; // SLT
-         4'b1001: result = ($unsigned(operand1) < $unsigned(operand2)) ? 1 : 0; // SLTU
+         ADD : result = operand1 +   operand2;
+         SUB : result = operand1 -   operand2;
+         AND : result = operand1 &   operand2;
+         OR  : result = operand1 |   operand2;
+         XOR : result = operand1 ^   operand2;
+         SLL : result = operand1 <<  operand2;
+         SRL : result = operand1 >>  operand2;
+         SRA : result = operand1 >>> operand2;
+         SLT : result = ($signed(operand1) < $signed(operand2)) ? 1 : 0;
+         SLTU: result = ($unsigned(operand1) < $unsigned(operand2)) ? 1 : 0;
          default: result = 0; // Default case
       endcase
       zero = (result == 0);
--- source/ex_core/ex_core_pkg.sv
+++ source/ex_core/ex_core_pkg.sv
@@ -1,5 +1,18 @@
 package ex_core_pkg;
 
+typedef enum logic [3:0] {
+    ADD  = 4'b0000,
+    SUB  = 4'b0001,
+    AND  = 4'b0010,
+    OR   = 4'b0011,
+    XOR  = 4'b0100,
+    SLL  = 4'b0101,
+    SRL  = 4'b0110,
+    SRA  = 4'b0111,
+    SLT  = 4'b1000,
+    SLTU = 4'b1001
+} t_alu_op;
+
 typedef struct packed {
     logic [4:0] RegSrc1;
     logic [4:0] RegSrc2;
