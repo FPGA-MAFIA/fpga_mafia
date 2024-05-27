6697ffbacd1645998a96dc31ec1228476bb160b7
amichai-bd <amichaibd@gmail.com>
2024-05-27 12:35:32 +0300

    Revert changes in FPGA directory to match the main branch revert gitignore chagnes Revert changes in PS2_alive.c and PS2_alive.txt to match the main branch

--- verif/big_core/tests/PS2_alive.c
+++ verif/big_core/tests/PS2_alive.c
@@ -6,6 +6,8 @@ int main(){
 
 char str[30];
 
+//clear_screen();  // uncomment that line when running only with -app for clean screen in FPGA
+
 rvc_printf("ENTER A STRING:\n> ");
 rvc_scanf(str, 30);
 rvc_printf("\nYOU ENTERED: ");
