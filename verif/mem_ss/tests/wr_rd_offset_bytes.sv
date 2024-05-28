83bf0a924e707c510328990515cb54491e30cf06
roman022285 <roman329@gmail.com>
2024-05-28 16:42:09 +0300

    modify test

--- verif/mem_ss/tests/wr_rd_offset_bytes.sv
+++ verif/mem_ss/tests/wr_rd_offset_bytes.sv
@@ -3,13 +3,19 @@
 // load cache
 delay(5);   wr_req(20'hE8_01_0, 32'hDEAD_BEAF , 5'b0); // write word
 delay(5);   wr_req_sh(20'hE9_01_1, 32'hAAAA_FEDC , 5'b1); // write half byte offset 1
-delay(5);   wr_req_sb(20'hE9_01_3, 32'hAAAA_AA12 , 5'b1); // write byte to offset 3
+delay(5);   wr_req_sb(20'hE9_01_3, 32'hAAAA_AAF2 , 5'b1); // write byte to offset 3
+delay(5);   wr_req_sb(20'hE9_01_7, 32'hAAAA_AADC , 5'b1); // write byte to offset 3 in word with offset 1
 
 delay(5);   rd_req(20'hE8_01_0, 5'd1);
-delay(5);   rd_req_lhu(20'hE9_01_1, 5'd2); 
-delay(5);   rd_req_lh(20'hE9_01_1, 5'd2);
+delay(5);   rd_req_lbu(20'hE9_01_3, 5'd1); 
+delay(5);   rd_req_lb(20'hE9_01_3, 5'd1);
+delay(5);   rd_req_lhu(20'hE9_01_1, 5'd1); 
+delay(5);   rd_req_lh(20'hE9_01_1, 5'd1);
 
-delay(5);   rd_req_lbu(20'hE8_01_3, 5'd1); 
-delay(5);   rd_req_lb(20'hE8_01_3, 5'd1);
+delay(5);   rd_req_lbu(20'hE9_01_7, 5'd2); 
+delay(5);   rd_req_lb(20'hE9_01_7, 5'd2);
+
+
+
 
 
