bf206e2db09ff3cb4f258b31c76c3dcaf535b863
roman022285 <roman329@gmail.com>
2024-07-23 21:25:30 +0300

    modify basic - test : add another accelerator check

--- verif/mini_core_accel/tests/accel_farm_int8_basic.c
+++ verif/mini_core_accel/tests/accel_farm_int8_basic.c
@@ -3,7 +3,7 @@
 // using int8 multipliers
 //=========================================================
 
-//./build.py -dut mini_core_accel -test accel_farm_int8_basic -app -hw -sim
+//./build.py -dut mini_core_accel -test accel_farm_int8_basic -app -hw -sim 
 
 #include "mini_core_accel_defines.h"
 #define WRITE_REG(REG,VAL) (*REG) = VAL
@@ -11,18 +11,25 @@
 
 int main() {
 
-    //Writing to multiplier #0
+    //Writing to multiplier #0, #1
 
     int data0 = 0x00000304; // {multiplicand = 3, multiplier = 4}
-    int continue_when_finish = 0x00000304;
+    int data1 = 0x0000fd04; // {multiplicand = -3, multiplier = 4}
     WRITE_REG(CR_CORE2MUL_INT8_0, data0);
+    WRITE_REG(CR_CORE2MUL_INT8_1, data1);
 
-    int polling = 0;
-    int mul_result0;
+    int polling0 = 0;
+    int polling1 = 0;
+    int mul_int8_result0, mul_int8_result1;
 
-    while(!polling) {
-        READ_REG(mul_result0, CR_MUL2CORE_INT8_0);
-        polling = (mul_result0 & 0x00010000) >> 16;  // extend ready bit
+    while(!polling0) {
+        READ_REG(mul_int8_result0, CR_MUL2CORE_INT8_0);
+        polling0 = (mul_int8_result0 & 0x00010000) >> 16;  // extend ready bit
+    }
+
+     while(!polling1) {
+        READ_REG(mul_int8_result1, CR_MUL2CORE_INT8_1);
+        polling1 = (mul_int8_result1 & 0x00010000) >> 16;  // extend ready bit
     }
 
     return 0;
