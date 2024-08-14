a7b58c5cf4191cc6a9d67e0d542c249ba3962dd3
ארז שקלים <erezshkalim@gmail.com>
2024-08-14 15:18:13 +0300

    removed nop. not needed

--- verif/accel_core/tests/accel_funcs.c
+++ verif/accel_core/tests/accel_funcs.c
@@ -3,10 +3,9 @@
 uint32_t *address = (uint32_t *)0x00FE0200;
 
 int xor_accel(int x, int y) {
-    int result;
+    int result = 0;
     WRITE_REG(address,x);
     WRITE_REG((address+1), y);
-    NOP();
-    READ_REG(result,address+2);
+    READ_REG(result,(address+2));
     return result;
 }
