3ce01195685f89e943c7e29cc8f4d0e79940140a
roman gilgor <roman329@gmail.com>
2024-08-17 19:50:02 +0300

    refactor naming

--- source/mini_core_accel/accelerators/pipe_line_mul.sv
+++ source/mini_core_accel/accelerators/pipe_line_mul.sv
@@ -16,22 +16,22 @@ import mini_core_accel_pkg::*;
     output logic [15:0] result
 );
 
-    logic [16:0] acc [7:0];
-    logic [16:0] next_acc [7:0];
-    logic [7:0]  pre_ready;
-    logic [16:0] pre_acc;
+    logic [16:0] acc [7:1];  
+    logic [16:0] next_acc [6:0];  // that signal do not used in the last stage
+    logic [7:0]  pipe_is_full;
+    logic [16:0] init_acc;
 
-    assign pre_ready[0] = (rst) ? 1'b0 : 1'b1;
-    assign ready        = pre_ready[7]; // onlu after 7 stages the result can be ready
+    assign pipe_is_full[0] = (rst) ? 1'b0 : 1'b1;
+    assign ready        = pipe_is_full[7]; // only after 8 stages the first result is ready
 
     // first stage
-    assign pre_acc = (rst) ? 'b0 : (start) ? {8'b0, multiplier, 1'b0} : 'b0;
-    assign next_acc[0] =  (pre_acc[1:0] == 2'b01) ? $signed({pre_acc[16:9]+multiplicand, pre_acc[8:1], pre_acc[0]}) >>> 1 :
-                          (pre_acc[1:0] == 2'b10) ? $signed({pre_acc[16:9]-multiplicand, pre_acc[8:1], pre_acc[0]}) >>> 1 :
-                                                                                                  $signed(pre_acc) >>> 1 ;
+    assign init_acc = (rst) ? 'b0 : (start) ? {8'b0, multiplier, 1'b0} : 'b0;
+    assign next_acc[0] =  (init_acc[1:0] == 2'b01) ? $signed({init_acc[16:9]+multiplicand, init_acc[8:1], init_acc[0]}) >>> 1 :
+                          (init_acc[1:0] == 2'b10) ? $signed({init_acc[16:9]-multiplicand, init_acc[8:1], init_acc[0]}) >>> 1 :
+                                                                                                  $signed(init_acc) >>> 1 ;
 
     `MAFIA_DFF(acc[1], next_acc[0], clk)
-    `MAFIA_DFF(pre_ready[1], pre_ready[0], clk)
+    `MAFIA_DFF(pipe_is_full[1], pipe_is_full[0], clk)
 
     genvar stage_num;
     generate
@@ -40,15 +40,14 @@ import mini_core_accel_pkg::*;
                                          (acc[stage_num][1:0] == 2'b10) ? $signed({acc[stage_num][16:9]-multiplicand, acc[stage_num][8:1], acc[stage_num][0]}) >>> 1 :
                                                                                                                                        $signed(acc[stage_num]) >>> 1 ;   
             `MAFIA_DFF(acc[stage_num+1], next_acc[stage_num], clk)
-            `MAFIA_DFF(pre_ready[stage_num+1], pre_ready[stage_num], clk)
+            `MAFIA_DFF(pipe_is_full[stage_num+1], pipe_is_full[stage_num], clk)
         end
     endgenerate
     
-    logic [16:0] pre_result;
-    assign pre_result =   (acc[7][1:0] == 2'b01) ? $signed({acc[7][16:9]+multiplicand, acc[7][8:1], acc[7][0]}) >>> 1 :
-                          (acc[7][1:0] == 2'b10) ? $signed({acc[7][16:9]-multiplicand, acc[7][8:1], acc[7][0]}) >>> 1 :
-                                                                                               $signed(pre_acc) >>> 1 ;
-    assign result = (!ready) ? 'b0 : pre_result[16:1];
+    assign acc[7] =  (acc[6][1:0] == 2'b01) ? $signed({acc[6][16:9]+multiplicand, acc[6][8:1], acc[6][0]}) >>> 1 :
+                     (acc[6][1:0] == 2'b10) ? $signed({acc[6][16:9]-multiplicand, acc[6][8:1], acc[6][0]}) >>> 1 :
+                                                                                           $signed(acc[6]) >>> 1 ;
+    assign result = (!ready) ? 'b0 : acc[7][16:1];
 
 endmodule 
 
