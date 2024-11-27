c883f96980d23806eba3e125be1c1c5a4295cd22
roman022285 <roman329@gmail.com>
2024-11-27 18:04:47 +0200

    demonstrate new branch

--- test.c
+++ test.c
@@ -1,29 +1,9 @@
-int main()  {
-    int a = 1;
-    int b = 2;
-    int c = 3;
-    int d = 4;
-    int e = 5;
-    int f = 6;
-    int g = 7;
-    int h = 8;
-    int i = 9;
-    int j = 10;
-    int k = 11;
-    int l = 12;
-    int m = 13;
-    int n = 14;
-    int o = 15;
-    int p = 16;
-    int q = 17;
-    int r = 18;
-    int s = 19;
-    int t = 20;
-    int u = 21;
-    int v = 22;
-    int w = 23;
-    int x = 24;
-    int y = 25;
-    int z = 26;
-    return 0;
-}
+    // test file
+
+    int main(){
+
+        int x = 10;
+
+        return 0;
+
+    }
