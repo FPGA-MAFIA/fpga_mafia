f9c1583e85536a8933672607d09f1605659e8e64
abdkeb <keblawi@campus.technion.ac.il>
2024-12-11 18:36:10 +0200

    cache update

--- source/ifu/ifu_cache.sv
+++ source/ifu/ifu_cache.sv
@@ -7,10 +7,12 @@ module ifu_cache(
 input logic Clock,
 input logic Rst,
 input logic [31:0] pc,
+input logic insLineIn,
+input logic insLineValidIn,
 
 // Outputs
-output logic [127:0] insLine,
-output logic insLineValid
+output logic [127:0] insLineOut,
+output logic insLineValidOut
 
 );
 
@@ -18,23 +20,36 @@ tag_arr_t tagArray [NUM_TAGS];
 data_arr_t dataArray [NUM_LINES];
 logic [TAG_WIDTH - OFFSET_WIDTH - 1 ] pcTag;
 
-assign pcTag = pc[TAG_WIDTH-1:OFFSET]
+assign pcTag = pc[TAG_WIDTH-1:OFFSET];
 
-`MAFIA_DFF_RST(tagArray.Valid,tagArray.Valid,Clock,Rst) // initiate Valid bits to 0 on Rst
+always_comb@ begin
+    if(Rst) begin
+        tagArray.Valid = '0;
+    end
+end
 
-
-always_ff@(posedge clk) begin
-
-    insLineValid <= 0;
-    for (int i = 0 ; i < NUM_TAGS ; i++) begin
-        if(!tagArray[i].Valid) begin
-            tagArray[i].evacuation <= 1;
-        end else begin
-            if(tagArray[i].tag == pcTag) begin
-                insLineValid <= 1;
-                insLine <= dataArray[i];
-                break;
+always_ff(posedge Clock) begin
+    
+    if (!insLineValidOut && insLineValidIn) begin
+        for (int i = 0 ; i < NUM_TAGS ; i++) begin
+            if (!tagArray[i].valid) begin    
+                dataArray[i] <= insLineIn;
+                tagArray[i].valid <= 1b'1;
+                tagArray[i].tag <= pcTag;
+                insLineValidOut <= 1b'1;
+                insLineOut <= insLineIn;
             end
+        end
+    end  
+end
+
+always_ff@(posedge Clock) begin
+
+    insLineValidOut <= 0;
+    for (int i = 0 ; i < NUM_TAGS ; i++) begin
+        if(tagArray[i].tag == pcTag) 
+            insLineValidOut <= tagArray[i].Valid;
+            insLineOut <= dataArray[i];
         end
     end
 end
