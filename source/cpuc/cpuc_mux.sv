07a1743d72e43f880559e564e1b4b04bba74322c
roman gilgor <roman329@gmail.com>
2025-01-28 14:42:20 +0200

    refactor mux sizes to fit DATA_WIDTH

--- source/cpuc/cpuc_mux.sv
+++ source/cpuc/cpuc_mux.sv
@@ -7,10 +7,10 @@ import cpuc_package::*;
 (
     input logic [DATA_WIDTH-1:0]   data_in0,
     input logic [DATA_WIDTH-1:0]   data_in1,
-    input logic                    ctrl,
+    input logic [DATA_WIDTH-1:0]   ctrl, // TODO - all the signals have the same width. reduce width may cause warning but probably not errors
     output logic [DATA_WIDTH-1:0]  data_out
 );
 
-    assign data_out = (!ctrl) ? data_in0 : data_in1; 
+    assign data_out = (ctrl == '0) ? data_in0 : (ctrl == 1) ? data_in1 : data_in0; 
 
 endmodule
