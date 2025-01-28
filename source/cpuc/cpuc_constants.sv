fa5b60447a58529215b02dcea758ab1c791517a8
roman gilgor <roman329@gmail.com>
2025-01-28 14:41:59 +0200

    refactor constants as memory

--- source/cpuc/cpuc_constants.sv
+++ source/cpuc/cpuc_constants.sv
@@ -4,17 +4,41 @@
 
 module cpuc_constants
 import cpuc_package::*;
-(
-    output logic [DATA_WIDTH-1:0]   const0,
-    output logic [DATA_WIDTH-1:0]   const1,
-    output logic [DATA_WIDTH-1:0]   const2,
-    output logic [DATA_WIDTH-1:0]   const3
+#(parameter CONST_NUM)
+(   
+    input logic                          clk,
+    input logic                          rst,
+    input logic [DATA_WIDTH-1:0]         constant_in,
+    input logic                          we,
+    input logic [$clog2(CONST_NUM)-1:0]  address,
+    output var t_constants_output        constants_output
 );
 
-    assign const0 = {{(DATA_WIDTH-1){1'b0}}, 1'b1};
-    assign const1 = {{(DATA_WIDTH-2){1'b0}}, 2'b10};
-    assign const2 = {{(DATA_WIDTH-2){1'b0}}, 2'b11};
-    assign const3 = {{(DATA_WIDTH-3){1'b0}}, 3'b100};
+    logic [DATA_WIDTH-1:0] mem [CONST_NUM-1:0];
+    logic [DATA_WIDTH-1:0] next_mem [CONST_NUM-1:0];
 
+    `CPUC_DFF(mem, next_mem, clk)
+
+    integer i;
+    always_comb begin
+        next_mem = mem;
+        if(rst) begin
+            for(i=0; i<CONST_NUM; i++) begin
+                next_mem[i] = '0;
+            end
+        end
+        else if(we) begin
+            next_mem[address] = constant_in;
+        end
+    end
+
+    genvar j;
+    generate 
+        for(j=0; j<CONST_NUM; j++) begin: generate_outputs
+            assign constants_output.constants_output[j] = mem[j]; 
+        end
+    endgenerate
+    
+    
 
 endmodule
