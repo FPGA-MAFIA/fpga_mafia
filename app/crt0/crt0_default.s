4bf55a7bd42ba5eb021c245e1d95f64d8d0061b9
unknown <zbiadssa@gmail.com>
2025-06-22 21:46:44 +0300

    testing: add,addi,sub,sw,lw

--- app/crt0/crt0_default.s
+++ app/crt0/crt0_default.s
@@ -10,35 +10,66 @@
 reset_handler:
   /* Initialize registers to zero (optional) */
   mv  x1, x0
+  mv  x1, x0
+  mv  x2, x1
   mv  x2, x1
   mv  x3, x1
+  mv  x3, x1
+  mv  x4, x1
   mv  x4, x1
   mv  x5, x1
+  mv  x5, x1
+  mv  x6, x1
   mv  x6, x1
   mv  x7, x1
+  mv  x7, x1
+  mv  x8, x1
   mv  x8, x1
   mv  x9, x1
+  mv  x9, x1
+  mv x10, x1
   mv x10, x1
   mv x11, x1
+  mv x11, x1
+  mv x12, x1
   mv x12, x1
   mv x13, x1
+  mv x13, x1
+  mv x14, x1
   mv x14, x1
   mv x15, x1
+  mv x15, x1
+  mv x16, x1
   mv x16, x1
   mv x17, x1
+  mv x17, x1
+  mv x18, x1
   mv x18, x1
   mv x19, x1
+  mv x19, x1
+  mv x20, x1
   mv x20, x1
   mv x21, x1
+  mv x21, x1
+  mv x22, x1
   mv x22, x1
   mv x23, x1
+  mv x23, x1
+  mv x24, x1
   mv x24, x1
   mv x25, x1
+  mv x25, x1
+  mv x26, x1
   mv x26, x1
   mv x27, x1
+  mv x27, x1
+  mv x28, x1
   mv x28, x1
   mv x29, x1
+  mv x29, x1
   mv x30, x1
+  mv x30, x1
+  mv x31, x1
   mv x31, x1
 
   /* Stack initialization */
@@ -74,7 +105,7 @@ jump_main:
 # Interrupt handler for the counter in location 
 ##################################################
 handle_interrupt:
-  .org 0x100
+  .org 0x200
     # Save registers on the stack
     addi sp, sp, -32     # Allocate stack space for 8 registers
     sw ra, 28(sp)        # Save return address
